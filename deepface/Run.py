8aa0533d06c78dd149f14225b12989fba5a026d3
blueprintparadise <rhiray03@gmail.com>
2021-09-17 11:54:03 +0530

    Update Run.py

--- deepface/Run.py
+++ deepface/Run.py
@@ -4,5 +4,5 @@ import cv2
 
 gpu_options = tf.compat.v1.GPUOptions(allow_growth=True)
 session = tf.compat.v1.InteractiveSession(config=tf.compat.v1.ConfigProto(gpu_options=gpu_options))
-DeepFace.stream(db_path=r"D:\DeepFace\deepface\deepface\images", model_name='Facenet', detector_backend='opencv',
+DeepFace.stream(db_path=r"./images", model_name='Facenet', detector_backend='opencv',
                 source=0, enable_face_analysis=False, time_threshold=1, frame_threshold=1)
